cc4347d8cb9871fe1137ab25792920b47f2457b9
lukobet <inlukobet@gmail.com>
2024-08-11 23:44:46 +0100

    worked

---
+++ practice with technana/day1.py
@@ -0,0 +1,14 @@
+print (20 + 30 * 50)
+
+print ("20 days are " + str(30) + " minutes")
+
+print (f"20 days are {20 * 24 * 60} minutes")
+
+#Variables are containers for storing values
+
+calculation_to_seconds = 24 * 60 * 60
+
+print (f"20 days are {20 * calculation_to_seconds} seconds")
+print (f"30 days are {30 * calculation_to_seconds} seconds")
+print (f"50 days are {50 * calculation_to_seconds} seconds")
+print (f"110 days are {110 * calculation_to_seconds} seconds")
