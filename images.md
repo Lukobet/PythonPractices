6bf6dbf038a6d5d3cb661fd9a1c99bc542bc443c
lukobet <inlukobet@gmail.com>
2024-09-14 14:38:09 +0100

    now with Mosh

--- images.md
+++ images.md
@@ -2,3 +2,20 @@ pictures from the practical
 
 ![Screenshot from 2024-08-11 21-35-30](https://github.com/user-attachments/assets/defe67d5-946f-4e9e-88c8-4a86c8267168)
 
+![Screenshot from 2024-08-11 22-14-46](https://github.com/user-attachments/assets/7789654d-16de-489f-a4fa-9f745ae8b847)
+
+![Screenshot from 2024-08-11 23-43-33](https://github.com/user-attachments/assets/de17d8d2-f56f-44e7-b614-fc80439fc8de)
+
+![Screenshot from 2024-08-11 23-55-48](https://github.com/user-attachments/assets/46f9fd8d-417d-4e4a-bc53-da1e3fddefb3)
+
+![Screenshot from 2024-08-23 05-58-04](https://github.com/user-attachments/assets/070f6b7e-1ce8-45e6-a909-874be941814b)
+
+![alt text](<Screenshot from 2024-09-14 14-32-09.png>)
+
+Day 2
+
+![alt text](<Screenshot from 2024-09-14 14-35-42.png>)
+
+
+
+
--- practice with technana/day1.py
+++ practice with technana/day1.py
@@ -12,3 +12,30 @@ print (f"20 days are {20 * calculation_to_seconds} seconds")
 print (f"30 days are {30 * calculation_to_seconds} seconds")
 print (f"50 days are {50 * calculation_to_seconds} seconds")
 print (f"110 days are {110 * calculation_to_seconds} seconds")
+
+#another example
+calculation_to_units = 24 * 60 * 60
+name_of_unit = "seconds"
+
+print (f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print (f"30 days are {30 * calculation_to_units} {name_of_unit}")
+print (f"50 days are {50 * calculation_to_units} {name_of_unit}")
+print (f"110 days are {110 * calculation_to_units} {name_of_unit}")
+
+
+#another example to days
+calculation_to_units = 24 
+name_of_unit = "hours"
+
+print (f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print (f"30 days are {30 * calculation_to_units} {name_of_unit}")
+print (f"50 days are {50 * calculation_to_units} {name_of_unit}")
+print (f"110 days are {110 * calculation_to_units} {name_of_unit}")
+
+
+
+print ("Tosin Aluko")
+
+print ("o----")
+print ('||||')
+print ('*' * 10)
